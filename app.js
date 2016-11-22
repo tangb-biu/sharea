ebca57a23348784b953b480b7667d9512e90def9
mc-tangb <511598230@qq.com>
2016-11-22 19:25:40 +0800

    nothing to say but update

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+var ejs = require('ejs');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.engine('html', ejs.renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
